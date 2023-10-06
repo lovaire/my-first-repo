54a64566e1ee1530b8ec1cf1d63ce7e091653dcc
lovaire <rifsrahmt@gmail.com>
2023-10-07 01:25:28 +0700

    alhamdulillah buat tugas

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, add_money, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_item, delete
+from main.views import show_main, add_money, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_item, delete, get_product_json, add_money_ajax, delete_by_ajax
 app_name = 'main'
 
 urlpatterns = [
@@ -13,5 +13,8 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
-    path('edit-product/<int:id>', edit_item, name='edit_item'),
+    path('edit-item/<int:id>', edit_item, name='edit_item'),
+    path('get-product/', get_product_json, name='get_product_json'),
+    path('add-money-ajax/', add_money_ajax, name='add_money_ajax'),
+    path('delete-by-ajax/', delete_by_ajax, name='delete_by_ajax'),
 ]
